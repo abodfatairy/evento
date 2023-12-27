2fd31fd977fa899793e669ac920a0d734cc4e5fa
abodfatairy <abodfatairy@gmail.com>
2023-12-27 14:29:38 +0200

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className=" flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-fixed bg-center bg-cover">
+            {children}
+        </div>
+    )
+}
+
+export default layout
---
+++ lib/mongoDb/database/index.tsx
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+const MONGODB_URI = process.env.MONGODB_URI
+let cached = (global as any).mongoose || { conn: null, Promise: null }
+
+export const connectToDatabase = async () => {
+    if (cached.conn) return cached.conn
+
+    if (!MONGODB_URI) throw new Error('data base is missing')
+    cached.Promise = cached.Promise || mongoose.connect(MONGODB_URI, {
+        dbName: 'evently',
+        bufferCommands: false,
+    })
+    cached.conn = await cached.Promise;
+    return cached.conn
+}
